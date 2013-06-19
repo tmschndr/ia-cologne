e0d27ab5b1cfe92330c4cbec60e369b344dd1eed
Tim Schneider <tim@railslove.com>
2013-06-19 15:32:11 +0200

    hackety hack #iacologne

--- ia-parallax.js
+++ ia-parallax.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
    $screen = $(".screen");
+   offset = 200;
    gyro.frequency = 1;
-
    gyro.startTracking(function(o){
-      factorTop  = parseInt(gyro.getOrientation().gamma, 10);
-      factorLeft = parseInt(gyro.getOrientation().beta, 10);
+      gyroGamma = parseInt(gyro.getOrientation().gamma, 10);
+      gyroBeta  = parseInt(gyro.getOrientation().beta, 10);
 
-      positionTop  = factorTop * -1;
-      positionLeft = factorLeft * -1;
+      positionTop  = (-0.5 * gyroGamma) - offset;
+      positionLeft = (-0.5 * gyroBeta) - offset;
 
       $screen.css("background-position", positionTop+"px " + positionLeft+"px");
-
-      console.log("positionTop: "+positionTop + " | positionLeft: "+positionLeft);
-
    });
 });
